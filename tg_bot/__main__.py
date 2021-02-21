27d292cef35e2404e6116528f89426cc25e46401
ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2021-02-22 00:56:24 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -29,7 +29,7 @@ PM_START_TEXT = """
 
 Hi {}, My name is {} !
 
-I am saber anime themed bot 
+I am Angella 🧚  anime themed bot 
 
 𝓘 𝓪𝓶 𝓽𝓱𝓮 𝓼𝔀𝓸𝓻𝓭 𝓱𝓮𝓻𝓸  𝓸𝓷𝓮 𝓸𝓯 𝓽𝓱𝓮 𝓗𝓮𝓻𝓸 𝓸𝓯 𝓱𝓸𝓵𝔂 𝓰𝓻𝓪𝓲𝓵 𝔀𝓪𝓻 𝓘 𝓪𝓶 𝓼𝓾𝓶𝓶𝓸𝓷𝓮𝓭 𝓫𝔂 𝓶𝔂 𝓶𝓪𝓼𝓽𝓮𝓻 𝓽𝓸 𝓱𝓮𝓵𝓹 𝓱𝓲𝓶
 
